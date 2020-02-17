bf9b3529988cf5f1151db9db146faa785d4a035a
Faith Oyedemi <faithoyedemi@yahoo.com>
2020-02-17 12:43:03 +0100

    initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  stories: ["../src/**/*.stories.js"],
+  addons: [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-a11y/register",
+    "@storybook/addon-knobs/register",
+    "@storybook/addon-actions/register",
+    "@storybook/addon-storysource"
+  ]
+};
